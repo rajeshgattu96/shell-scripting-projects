467d98346c49d8e641e28dc7e52fcd79c2744580
Rajesh Gattu <rajeshgattuaws@gmail.com>
2025-01-09 16:10:53 +0530

    Update nodehealth.sh

--- nodehealth.sh
+++ nodehealth.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
+###########################
+# Author: Rajesh Gattu
+# Date: 09/01/2025
+
+# This script outputs the node health
+# Vesrion: 0.0.1
+########################
+
+set -x
+
+# Disk space
+echo " Print the disk space"
+df -h
+# Memory
+echo " Print the memory"
+free -g
+
+# CPU
+echo" Print the CPU"
+nproc
+
+
 
